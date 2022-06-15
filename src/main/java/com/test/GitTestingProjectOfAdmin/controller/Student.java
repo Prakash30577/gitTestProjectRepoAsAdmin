fdb959db67c07c991b2dbd41e998f1105b706488
omewada <omewada@192.168.50.98>
2022-06-15 12:22:52 +0530

    Some New Changes...

--- src/main/java/com/test/GitTestingProjectOfAdmin/controller/Student.java
+++ src/main/java/com/test/GitTestingProjectOfAdmin/controller/Student.java
@@ -2,5 +2,9 @@ package com.test.GitTestingProjectOfAdmin.controller;
 
 @SuppressWarnings(value = { "" })
 public class Student {
+	
+	public static void main(String[] args) {
+		System.out.println("Hii");
+	}
 
 }
